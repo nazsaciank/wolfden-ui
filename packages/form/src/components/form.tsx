4f95251676f5b3bd41b1dc1853e90a11fff9de87
Nazareno Ciancaglini <nazareno.ciancaglini@gmail.com>
2025-05-26 11:17:00 -0300

    chore: Form component to remove forwardRef

--- packages/form/src/components/form.tsx
+++ packages/form/src/components/form.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { PolymorphicProps } from "@wolfden-ui/system"
-import { ElementType, FormEvent, ForwardedRef, forwardRef, useMemo } from "react"
+import { ElementType, FormEvent, useMemo } from "react"
 import { FormContext } from "../context"
 import { FormGroup } from "../types"
 
@@ -10,7 +10,7 @@ export type FormProps<E extends ElementType, T extends object = any> = Omit<Poly
 	onSubmit?: (values: T) => void
 }
 
-export const Form = forwardRef(function Form<E extends ElementType = "form", F = HTMLFormElement>({ as, group, onSubmit, children, ...props }: FormProps<E>, ref: ForwardedRef<F>) {
+export function Form<E extends ElementType = "form">({ as, group, onSubmit, children, ...props }: FormProps<E>) {
 	const As = useMemo(() => as || "form", [as])
 
 	function handleOnSubmit(ev: FormEvent<HTMLFormElement>) {
@@ -18,13 +18,12 @@ export const Form = forwardRef(function Form<E extends ElementType = "form", F =
 
 		if (onSubmit) onSubmit(group.values)
 	}
-
 	return (
 		<FormContext.Provider value={{ findControl: group?.find }}>
-			{/* @ts-expect-error - ref is not supported on polymorphic components */}
-			<As ref={ref} onSubmit={handleOnSubmit} {...props}>
+			{/* @ts-ignore */}
+			<As onSubmit={handleOnSubmit} {...props}>
 				{children}
 			</As>
 		</FormContext.Provider>
 	)
-})
+}
